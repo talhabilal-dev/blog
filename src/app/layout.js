c1304e2737fe9505a0e31f0b8d9eb421ba2c7cf9
talha-bilal-dev <Muhammad.talha.bilal.dev@gmail.com>
2025-03-25 20:58:46 +0500

    initialize Next.js project with Tailwind CSS and essential configurations

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Full Stack Blog",
+  description:
+    "A full stack blog using Next.js, Tailwind CSS, and Lucide icons",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
